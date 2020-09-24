d61d1034b7fa9b411794107155188174231db78c
Irina <irina@mymail.com>
2020-09-24 23:46:57 +0700

    Implemented 2 methods for getMaxHeight in QuantTreeImpl: by loop and by strem Enabled QuantTreeImplTest

--- lecture02/src/main/java/io/github/javaasasecondlanguage/lecture02/practice/tree/quant/QuantTreeImpl.java
+++ lecture02/src/main/java/io/github/javaasasecondlanguage/lecture02/practice/tree/quant/QuantTreeImpl.java
@@ -10,6 +10,31 @@ public class QuantTreeImpl extends AbstractTree<Integer> implements QuantTree {
 
     @Override
     public int getMaxHeight() {
-        throw new RuntimeException("Not Implemented");
+
+        //return getHeight(root);
+        return getHeightLoop(root);
+
+    }
+
+    public static int getHeight(TreeNode<Integer> node) {
+        if (node == null) return 0;
+
+        return 1 + node.getChildren().stream()
+                .map(QuantTreeImpl::getHeight)
+                .max(Integer::compareTo)
+                .orElse(0);
+    }
+
+    public static int getHeightLoop(TreeNode<Integer> node) {
+        if (node == null) return 0;
+
+        int maxHeight = 0;
+        for (TreeNode<Integer> cur_node : node.getChildren()) {
+            int curMaxHeight = getHeightLoop(cur_node);
+            if (curMaxHeight > maxHeight) {
+                maxHeight = curMaxHeight;
+            }
+        }
+        return maxHeight + 1;
     }
 }
