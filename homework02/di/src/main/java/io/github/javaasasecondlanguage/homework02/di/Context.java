e91cd54f03a1537a8627987505a48e5f300bce7f
Irina <irina@mymail.com>
2020-10-06 23:31:15 +0700

    checked null arguments in Context and Injector added context cleanup to Context constructor

--- homework02/di/src/main/java/io/github/javaasasecondlanguage/homework02/di/Context.java
+++ homework02/di/src/main/java/io/github/javaasasecondlanguage/homework02/di/Context.java
@@ -10,13 +10,29 @@ public class Context {
     static final List<Object> context = new ArrayList<>();
     static final Map<String, Object> qContext = new HashMap<>();
 
+    // TODO it is strange decision, but now it is: if Someone create new Context
+    // all previous registered objects will be removed
+    public Context() {
+        context.clear();
+        qContext.clear();
+    }
+
     public <T> Context register(T object, String qualifier) {
+        if (qualifier == null) {
+            throw new IllegalArgumentException("Context qualifier should be not null");
+        }
+        if (object == null) {
+            throw new IllegalArgumentException("Context object should be not null");
+        }
         qContext.put(qualifier, object);
         register(object);
         return this;
     }
 
     public <T> Context register(T object) {
+        if (object == null) {
+            throw new IllegalArgumentException("Context object should be not null");
+        }
         final int index = context.indexOf(object);
         if (index != -1) {
             context.remove(index);
--- homework02/di/src/main/java/io/github/javaasasecondlanguage/homework02/di/Injector.java
+++ homework02/di/src/main/java/io/github/javaasasecondlanguage/homework02/di/Injector.java
@@ -1,10 +1,19 @@
 package io.github.javaasasecondlanguage.homework02.di;
 
-public class Injector {
+public final class Injector {
+
+    private Injector() {
+    }
+
     public static <T> T inject(Class<T> clazz) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("Class for inject should be not null");
+        }
+
         final Object[] objects = Context.context.stream()
                 .filter(object -> isObjectCastToClass(object, clazz))
                 .toArray();
+
         if (objects.length > 1) {
             throw new RuntimeException("Too many object found");
         }
@@ -17,16 +26,22 @@ public class Injector {
     }
 
     public static <T> T inject(Class<T> clazz, String qualifier) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("Class should be not null");
+        }
+
+        if (qualifier == null) {
+            throw new IllegalArgumentException("Qualifier should be not null");
+        }
+
         final Object obj = Context.qContext.get(qualifier);
         if (obj == null) {
-            //TODO change Exceptions
             throw new RuntimeException("Can't find object with qualifier %s".formatted(qualifier));
         }
         if (isObjectCastToClass(obj, clazz)) {
             return (T) obj;
         }
 
-        //TODO change Exceptions
         throw new RuntimeException("Qualifier %s is not good for this class".formatted(qualifier));
     }
 
