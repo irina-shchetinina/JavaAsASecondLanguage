d1bf91b12903db4ac90609fb717b193b0a06dabf
Irina <irina@mymail.com>
2020-10-24 22:55:32 +0700

    lecture practice: Implementing ChatController

--- lecture04/chat/src/main/java/io/github/javaasasecondlanguage/chat/server/ChatController.java
+++ lecture04/chat/src/main/java/io/github/javaasasecondlanguage/chat/server/ChatController.java
@@ -61,15 +61,52 @@ public class ChatController {
     /**
      * curl -X POST -i localhost:8080/chat/logout -d "name=I_CAN_COPY_PASTE"
      */
-    //TODO
+    @RequestMapping(
+            path = "logout",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> logout(@RequestParam("name") String name) {
+        if (!usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("User " + name + " isn't logged in");
+        }
+        usersOnline.remove(name);
+        messages.add("[" + name + "] logged out");
+        return ResponseEntity.ok().build();
+    }
+
 
     /**
      * curl -X POST -i localhost:8080/chat/say -d "name=I_CAN_COPY_PASTE&msg=Hello"
      */
-    //TODO
+    @RequestMapping(
+            path = "say",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> say(@RequestParam("name") String name,
+                                      @RequestParam("msg") String msg) {
+        if (!usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("User " + name + " isn't logged in");
+        }
+        //TODO some checks for correct message without inj maybe
+        messages.add("[" + name + "] " + msg);
+        return ResponseEntity.ok().build();
+    }
+
 
     /**
      * curl -i localhost:8080/chat/chat
      */
-    //TODO
+    @RequestMapping(
+            path = "chat",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<?> chat() {
+        String responseBody = messages.stream()
+                //.sorted()
+                .collect(Collectors.joining("\n"));
+        return ResponseEntity.ok(responseBody);
+    }
+
 }
